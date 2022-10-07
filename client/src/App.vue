54fd29c388d966131d44efabe379b5c77dd35174
Nick Nash <nicholas.nash@cashtn.com>
2022-10-07 14:47:24 -0500

    built a quick scaffold of a Vue front-end UI

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <LogCollector/>
+</template>
+
+<script>
+import LogCollector from './components/LogCollector.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LogCollector
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding-top: 60px;
+  min-height: 100vh;
+}
+</style>
